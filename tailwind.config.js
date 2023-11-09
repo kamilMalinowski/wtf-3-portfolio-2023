d3e38a60aa074968cf6b0cda8c6df082062c4547
Kamil Malinowski <universerkmp@gmail.com>
2023-11-09 23:04:53 +0100

    Add fonts and colors configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'dmsans': ['DM Sans', 'sans-serif']
+    },
+    colors:{
+      primary: '#CEFF7E',
+      gray: {
+        400: '#9CA3AF',
+        700: '#1F2937',
+        900: '#020916'
+      }
+    },
     extend: {},
   },
   plugins: [],
